cf693114e998aa9db825f8c5dfba9315aafd3ce1
SilviaP46 <61684236+SilviaP46@users.noreply.github.com>
2020-03-05 12:01:09 +0200

    try

--- incercare/Source.cpp
+++ incercare/Source.cpp
@@ -1,5 +1,21 @@
 #include<iostream>
 using namespace std;
+
+bool test_prim(int n)
+{	
+
+	if (n <= 1)
+	return false;
+
+	for (int i = 2;i <= float(n / 2);i++)
+		if (n % i == 2)
+			return false;
+	return true;
+}
 int main() {
-	
+	int n;
+	if (test_prim(5))
+	std::cout << "ok bitch";
+		return 0;
+
 }
